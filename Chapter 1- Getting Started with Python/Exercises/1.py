a045008a721d4d9b3837c86adfd6da9b5b9d6169
Keira-L4-Python <145338953+Keira-L4-Python@users.noreply.github.com>
2024-01-19 00:13:21 +0400

    update

--- Chapter 1- Getting Started with Python/Exercises/1.py
+++ Chapter 1- Getting Started with Python/Exercises/1.py
@@ -1,9 +1,8 @@
-One= "Twinkle, twinkle little star,"
+print("Twinkle twinkle little star,")
+print("\tHow I wonder what you are,")
+print("\t\tUp above the world so high,") 
+print("\t\tLike a diamond in the sky,")
+print("Twinkle twinkle little star,")
+print("\tHow I wonder what you are,")
 
-Two= "How I wonder what you are!"
-
-Three= "Up above the world so high"
-
-Four= "Like a diamond in the sky"
-
-print(One, "\n ", Two, "\n ", Three, "\n ", Four, "\n ", One, "\n ", Two)
+#Twinkle twinkle little star
